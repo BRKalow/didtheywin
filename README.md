bb05596ef982b8a9c222ab32e3a3f257488a4197
Bryce <br.kalow@gmail.com>
2014-01-15 15:51:37 -0600

    README file added

---
+++ README.md
@@ -0,0 +1,18 @@
+DidTheyWin?
+==========
+
+A one page Sinatra app to see if an NBA won last night.
+Created by Bryce Kalow, http://brycekalow.name
+Version 1.0
+
+Usage
+-----
+
+From the root of the app, run:
+`ruby app.rb`
+
+Credits
+-------
+
+Data provided by the xmlstats API:
+http://erikberg.com/api
--- lib/settings.rb
+++ lib/settings.rb
@@ -4,21 +4,12 @@ module Settings
 
   extend self
 
-
   @_settings = {}
   attr_reader = :_settings
 
   def load!(filename)
     newsets = YAML::load_file(filename)
     @_settings.merge! newsets
-  end
-
-  # Deep merging of hashes
-  # deep_merge by Stefan Rusterholz, see http://www.ruby-forum.com/topic/142809
-  def deep_merge!(target, data)
-    merger = proc{|key, v1, v2|
-      Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
-    target.merge! data, &merger
   end
 
   def method_missing(name, *args, &block)
