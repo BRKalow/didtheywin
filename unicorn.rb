717dd153467a4322df1b4fafa9b65bc82741467a
Bryce <br.kalow@gmail.com>
2014-07-28 22:11:25 -0500

    Removed uneeded space

--- unicorn.rb
+++ unicorn.rb
@@ -12,7 +12,7 @@ timeout 30
 preload_app true
 
 before_exec do |server|
-    ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
+    ENV['BUNDLE_GEMFILE'] = File.expand_path('./Gemfile', File.dirname(__FILE__))
 end
 
 before_fork do |server, worker|
